231cde12a2640ae1fba7a1370c2d9facd10f756c
Gavriel-Cohen <gavriel.cohen@live.biu.ac.il>
2025-04-21 21:39:27 +0300

    header and tests for storage interface

---
+++ src/Istorage.h
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <optional>
+
+using std;
+template <typename T>
+class IStorage {
+public:
+    virtual ~IStorage() = default;
+    
+    // Save data to storage
+    virtual bool save(const T& data) = 0;
+    
+    // Load data from storage
+    virtual optional<T> load() = 0;
+    
+    // Check if data exists in storage
+    virtual bool exists() const = 0;
+};
