25e09290a1eda007b2ac8ad1d1b442a19557ace7
OmerElmaliach <omerelmaliachprivate@gmail.com>
2025-04-24 15:41:35 +0300

    AE-67 Added run and helper functions

--- src/datatypes/cli.cpp
+++ src/datatypes/cli.cpp
@@ -1,7 +1,52 @@
+#include <string>
+#include <iostream>
+#include <regex>
+#include <bits/stdc++.h>
 #include "imenu.h"
+#include "bloomFilterStorage.h"
 using namespace std;
 
-class CLI : public IMenu<int>{
+class CLI : public IMenu<int, string>{
+private:
+    /**
+     * @brief Helper function, returns true if input string is in proper format for bloom filter settings input, otherwise false.
+     * 
+     * @param input String input to be checked.
+     */
+    bool checkInput(string input) {
+        static regex bloomInputRegex("(\\d+\\s+)+");
+        return regex_match(input, bloomInputRegex);
+    }
+
+
+    /**
+     * @brief Helper function, returns true if input string is in proper format of {number command} {url}, otherwise false.
+     * 
+     * @param input String input to be checked.
+     */
+    bool checkRegex(string input) {
+        static regex urlRegex("^\\d+\\s+((https?:\\/\\/)?(www\\.)?([a-zA-Z0-9-]+\\.)+[a-zA-Z0-9]{2,})(\\/\\S*)?$");
+        return regex_match(input, urlRegex);
+    }
+
+
+    /**
+     * @brief Helper function, Returns a vector of strings previously seperated by a delimiter.
+     * 
+     * @param input String input to be checked.
+     */
+    vector<string> split(string str, char delimiter) {
+        vector<string> vec;
+        stringstream ss(str);
+        string substring;
+        while(getline(ss, substring, delimiter)) {
+            vec.push_back(substring);
+        }
+
+        return vec;
+    }
+
+
 public:
     /**
      * @brief CLI Builder function, defines the menu's state and the ICommands associated with command numbers.
@@ -16,6 +61,9 @@ public:
 
         // Menu has not started running yet.
         m_menuState = false;
+
+        // Initialize storage.
+        m_Stor = new bloomFilterStorage();
     }
 
 
@@ -23,7 +71,34 @@ public:
      * @brief Initializes the inteface of the menu and handles input.
      */
     void run() override {
-        // TODO
+        m_menuState = true;
+        while (isRunning()) {
+            string input;
+            if (!m_Stor.exists()) {
+                // Storage files do not not exist for bloom filter input.
+                do {
+                    // Loop until input is in the proper format.
+                    cin >> input;
+                } while(!checkInput(input));
+
+                vector<string> str_vec = split(input, ' ');
+                vector<int> input_vec;
+                for (string sub : str_vec) {
+                    input_vec.push_back(stoi(sub));
+                }
+                // Save the bloom input settings.
+                m_Stor.save(input_vec);
+            }
+
+            do {
+                // Loop until input is in the proper format for performing commands.
+                cin >> input;
+            } while(!checkRegex(input));
+            vector<string> str_vec = split(input, ' ');
+            int cmd = stoi(str_vec[0]);
+            // Execute the command associated with num in map.
+            executeCommand(cmd, str_vec[1]);
+        }
     }
 
 
@@ -34,7 +109,10 @@ public:
      * @param command Command to be executed.
      */
     void registerCommand(int& input, ICommand* command) override {
-        m_cmdMap.insert({input, command});
+        if (m_cmdMap.find(input) == m_cmdMap.end()) {
+            // Command number was not found in map, can insert.
+            m_cmdMap.insert({input, command});
+        }
     }
 
 
@@ -43,8 +121,11 @@ public:
      * 
      * @param input Input number to trigger a command.
      */
-    void executeCommand(int& input) override {
-        m_cmdMap[input]->execute();
+    void executeCommand(int& input, string& str) override {
+        if (m_cmdMap.find(input) != m_cmdMap.end()) {
+            // Command number was found in map, can perform command.
+            m_cmdMap[input]->execute(str);
+        }
     }
 
 
