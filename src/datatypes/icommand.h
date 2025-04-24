0e6f39e60465d8d9459382137a7f960a47ed98fc
etlbro <eazbrody@gmail.com>
2025-04-24 12:53:32 +0300

    adding icommad and addURL header

---
+++ src/datatypes/icommand.h
@@ -0,0 +1,56 @@
+
+
+
+
+using namespace std;
+
+// creating the abstract class for our command design pattern
+class Icommand {
+
+
+
+
+
+
+
+
+public:
+//TODO: should exe have a bool return value to make sure it worked?
+virtual void executeCommand()=0;
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
