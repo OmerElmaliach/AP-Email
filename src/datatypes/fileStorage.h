fec74707f3b3b6df2e25d219fc450570ae3f4af9
Gavriel-Cohen <gavriel.cohen@live.biu.ac.il>
2025-04-24 11:13:17 +0300

    moved to datatypes

---
+++ src/datatypes/fileStorage.h
@@ -0,0 +1,31 @@
+#include "Istorage.h"
+#include <fstream>
+#include <filesystem>
+#include <stdexcept>
+
+using namespace std;
+template <typename T>
+
+class FileStorage : public IStorage<T> {
+private:
+    mutable fstream fileStream;
+    const string filePath;
+
+    // Helper function to serialize an object to the file
+    void saveToFile(const T& object) const = 0;
+
+    // Helper function to deserialize an object from the file
+    T loadFromFile() const = 0;
+
+public:
+    explicit FileStorage(const string& fileName)
+        : filePath("../data/" + fileName);
+
+    void save(const T& object) override;
+
+    T load() override;
+
+    void remove() override;
+
+    bool exists() const override;
+};
