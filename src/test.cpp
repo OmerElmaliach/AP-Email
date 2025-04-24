5b65289d5809da5fe2b3016d8b4a1b5285a31c1c
OmerElmaliach <omerelmaliachprivate@gmail.com>
2025-04-24 16:48:19 +0300

    AE-63 Fixed test logic

--- src/test.cpp
+++ src/test.cpp
@@ -3,7 +3,7 @@
 
 // Should pass if an IMenu object was created.
 TEST(AppTest, objectCreation) {
-    App app = new App();
+    App* app = new App();
     std::cout << "App created\n";
     delete app;
     std::cout << "App destroyed\n";
