81f9a8ab1aec820363ccc1bc8df5ff17316e9515
OmerElmaliach <omerelmaliachprivate@gmail.com>
2025-04-27 17:41:57 +0300

    AE-57 Added volume

--- src/start.sh
+++ src/start.sh
@@ -2,6 +2,17 @@
 # DOCKER FOR RUNNING MAIN
 DOCKERFILE="DockerMain"
 
+# Build the image
 docker build -f ${DOCKERFILE} -t docker-main .
-docker run docker-main
-docker image rm -f docker-main
+
+# Create a directory outside of src named data if doesnt exist.
+cd ..
+prev_pwd=$(pwd)
+mkdir -p ./data
+cd src
+
+# Run and create a volume for data storage.
+docker run -v "$prev_pwd/data:/Ap_Email/data" docker-main
+
+# Delete the image previously created.
+docker image rm -f docker-maina
