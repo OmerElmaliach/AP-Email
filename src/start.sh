d41994ebe8316595a094ad38c7baa2049a74f76c
OmerElmaliach <omerelmaliachprivate@gmail.com>
2025-04-28 00:56:35 +0300

    AE-14 Fixed crash issue

--- src/start.sh
+++ src/start.sh
@@ -12,7 +12,7 @@ mkdir -p ./data
 cd src
 
 # Run and create a volume for data storage.
-docker run -v "$prev_pwd/data:/Ap_Email/data" docker-main
+docker run -it -v "$prev_pwd/data:/Ap_Email/data" docker-main
 
 # Delete the image previously created.
 docker image rm -f docker-main
