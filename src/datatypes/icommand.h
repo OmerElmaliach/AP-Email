423443c1bf0bdcf8436838f04af4618a60a42e98
etlbro <eazbrody@gmail.com>
2025-04-27 15:38:08 +0300

    added ifndef to icommand

--- src/datatypes/icommand.h
+++ src/datatypes/icommand.h
@@ -1,5 +1,6 @@
 
-
+#ifndef ICOMMAND_H
+#define ICOMMAND_H
 
 // creating the abstract class for our command design pattern
 class Icommand {
@@ -11,8 +12,8 @@ virtual ~Icommand() = default;
 // virtual command methode, the diffrenr command will implament
 virtual void executeCommand()=0;
 
-
 };
+#endif // ICOMMAND_H
 
 
 
