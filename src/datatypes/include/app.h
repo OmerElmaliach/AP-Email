eba4b4ec56c2e35f43c43cda5bb902e2fbbbf426
OmerElmaliach <omerelmaliachprivate@gmail.com>
2025-05-08 15:33:08 +0300

    AE-118  Changed Bloomfilter to Bloomfilterstorage in header

--- src/datatypes/include/app.h
+++ src/datatypes/include/app.h
@@ -1,23 +1,16 @@
 #ifndef APP_H
 #define APP_H
-#include <BloomFilter.h>
-#include <string>
-#include <MyHash.h>
+#include <bloomFilterStorage.h>
 
 class App {
 public:
     /**
-     * @brief Initializes an imenu type object and runs it.
-     */
-    void run();
-
-    
-    /**
      * @brief Initializes an imenu type object and runs it, supports socket.
      * 
      * @param sock Active socket.
+     * @param p_bloomFilterStorage Pointer to a bloom filter storage.
      */
-    void run(int sock, BloomFilter<string, MyHash> bloomFilter);
+    void run(int sock, bloomFilterStorage* p_bloomFilterStorage);
 };
 
 #endif
