7ea281dc2b141c42e1f661e57dd42deeffc000a8
Gavriel-Cohen <gavriel.cohen@live.biu.ac.il>
2025-05-08 23:00:58 +0300

    removed prints

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,9 @@
 // Main function, runs the run function inside app object.
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
         return 1;
     }
     if (!Server::checkValidInput(argv[1])) {
-        std::cerr << "Invalid port number. Please provide a valid port number." << std::endl;
         return 1;
     }
     Server server((int) argv[1]); // Create a server object with the given port number
