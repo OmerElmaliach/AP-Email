0e976f2628442c9ddcc3196ef7175e5511ad15f9
Gavriel-Cohen <gavriel.cohen@live.biu.ac.il>
2025-05-09 16:05:52 +0300

    fixed errors related to server running - bllom filter passed as pointer, docker not recieving args and more

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,18 @@
 // Main function, runs the run function inside app object.
 int main(int argc, char* argv[]) {
     if (argc < 2) {
+        for (size_t i = 0; i < argc; i++)
+        {
+            printf("%s\n", argv[i]); // Print all arguments passed to the program
+        }
+        
         return 1;
     }
     if (!Server::checkValidInput(argv[1])) {
+        printf("0\n");
         return 1;
     }
-    Server server(atoi(argv[1])); // Create a server object with the given port number
+    Server server(std::stoi(argv[1])); // Create a server object with the given port number
     server.startServer(); // Start the server
     while (true) {  // Server never stops working
         server.acceptAndHandleClient(); // Accept and handle client connections
