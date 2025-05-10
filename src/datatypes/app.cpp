bb9e6ccb4c7da9ea5fe9fa65ec182c6a6d98dc42
OmerElmaliach <omerelmaliachprivate@gmail.com>
2025-05-10 20:11:34 +0300

    AE-92 Fixed header mistake in app for CLI

--- src/datatypes/app.cpp
+++ src/datatypes/app.cpp
@@ -1,7 +1,7 @@
 #include <app.h>
 #include <cli.h>
 
-void App::run(int sock, bloomFilterStorage* p_bloomFilterStorage) {
+void App::run(int sock, bloomFilterStorage& p_bloomFilterStorage) {
     // Initialize CLI menu and run it.
     CLI* menu = new CLI(sock, p_bloomFilterStorage);
     menu->run();
