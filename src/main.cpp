afc751ae9a2469aea787d83d38ca2524c03de193
OmerElmaliach <omerelmaliachprivate@gmail.com>
2025-05-11 22:30:04 +0300

    AE-120 Added bloom filter save

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 // Main function, runs the run function inside app object.
 int main(int argc, char* argv[]) {
+    vector<int> args_filter;
     if (argc < 2) {
         for (size_t i = 0; i < argc; i++)
         {
@@ -14,7 +15,15 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     Server server(std::stoi(argv[1])); // Create a server object with the given port number
-    server.startServer(); // Start the server
+    for (int i = 2; i < argc; i++) {
+        int is_number = atoi(argv[i]);
+        // If is_number = 0, invalid input.
+        if (!is_number)
+            exit(1);
+        
+        args_filter.push_back(is_number);
+    }
+    server.startServer(args_filter); // Start the server
     while (true) {  // Server never stops working
         server.acceptAndHandleClient(); // Accept and handle client connections
     }
