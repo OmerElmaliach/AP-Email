d64acf1db851e6d9dcba655aa4fdf666aee07201
Gavriel-Cohen <gavriel.cohen@live.biu.ac.il>
2025-05-12 09:37:26 +0300

    fixed port number validation

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,20 @@
 int main(int argc, char* argv[]) {
     vector<int> args_filter;
     if (argc < 2) {
-        for (size_t i = 0; i < argc; i++)
-        {
-            printf("%s\n", argv[i]); // Print all arguments passed to the program
-        }
-        
-        return 1;
+        // incorrect number of arguments
+        exit(1);
     }
     if (!Server::checkValidInput(argv[1])) {
-        return 1;
+        // invalid port number
+        exit(1);
     }
     Server server(std::stoi(argv[1])); // Create a server object with the given port number
     for (int i = 2; i < argc; i++) {
+        // check bloom filter settings are numbers
         int is_number = atoi(argv[i]);
         // If is_number = 0, invalid input.
         if (!is_number)
             exit(1);
-        
         args_filter.push_back(is_number);
     }
     server.startServer(args_filter); // Start the server
