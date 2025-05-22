c8af09b1c18ca9ab57311d69c8967a482051b7ec
OmerElmaliach <omerelmaliachprivate@gmail.com>
2025-05-22 22:39:53 +0300

    AE-137 Added getUserMails command without verification

--- src/js/controllers/mails.js
+++ src/js/controllers/mails.js
@@ -1,3 +1,13 @@
-const Article = require('../models/mails');
+const Mails = require('../models/mails');
 
-// TODO: COMMANDS
+/**
+ * Returns fifty existing mails in reverse order by Id.
+ *
+ * @param {number} req - Request.
+ * @param {number} res - Response.
+ * @returns {number} Fifty Mails in json structure.
+ */
+exports.getUserMails = (req, res) => {
+    const { id } = req.body;
+    res.json(Mails.getUserMails(id));
+}
