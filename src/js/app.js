b9d92c537ebefeae6fb6bd539cf16a9aaeb97926
Gavriel-Cohen <gavriel.cohen@live.biu.ac.il>
2025-05-28 10:09:38 +0300

    implemented label functions

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,14 @@ const app = express();
 
 //lis of local js includes
 const mails = require('./routes/mails');
-const users = require('./routes/users')
+const users = require('./routes/users');
+const users = require('./routes/labels');
 
 
 app.use(express.json())
 //app.use('/api/mails', mails);
-app.use('/api/users', users)
+app.use('/api/users', users);
+app.use('/api/labels', labels);
 
 
 app.listen(9000);
