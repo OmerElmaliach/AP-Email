7b6194531afb6329d378533b9e57b2527f5b90b9
Gavriel-Cohen <gavriel.cohen@live.biu.ac.il>
2025-05-28 16:15:24 +0300

    updated labels

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+//list of local js includes
+const mails = require('./routes/mails');
+const users = require('./routes/users');
+const users = require('./routes/labels');
+
+
+app.use(express.json())
+//app.use('/api/mails', mails);
+app.use('/api/users', users);
+app.use('/api/labels', labels);
+
+
+app.listen(9000);
