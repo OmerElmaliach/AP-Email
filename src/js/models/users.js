542af806a296155c05bcea3473d47bc7bd3dc1b3
etlbro <eazbrody@gmail.com>
2025-05-29 18:41:30 +0300

    final implamentation for users route

--- src/js/models/users.js
+++ src/js/models/users.js
@@ -10,14 +10,30 @@ const addUser = (user) =>{
     users_DB.push(user)
 }
 
-/* function to get a user- enter key *string* to search and what val for that key 
-   **MUST CHECK RETURN VAL** */
+  /* function to get a user- enter key *string* to search and what val for that key 
+   **MUST CHECK RETURN VAL** 
+   */
 const getUser =  (field, value ) =>{
     return users_DB.find(user => user[field] === value)
 }
 
-// for debugging- all users
+/*
+updates a field of a user in users_DB
+parameters:
+id: User ID to find
+field: Field name to update (string)
+valueUpdate: New value for the field
+*/
+const updateValue = (id , field, valueUpdate)=>{
+  const updatedUser = users_DB.find(user => user.id === id)
+  if(updatedUser){
+  updatedUser[field] = valueUpdate
+}
+}
+
+/* for debugging- get all user
 const getAllUsers = () => {
   return users_DB
-}
-module.exports= { addUser, getUser ,         getAllUsers}
+}*/
+
+module.exports= { addUser, getUser ,updateValue}
