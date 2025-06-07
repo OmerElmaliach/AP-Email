61831cbba47df2c18e54bcde10719514830412a0
OmerElmaliach <omerelmaliachprivate@gmail.com>
2025-06-07 23:31:59 +0300

    AE-155 Page layout / CSS

--- src/react-server/src/App.js
+++ src/react-server/src/App.js
@@ -1,7 +1,39 @@
 import './App.css';
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <div className="topbar">
+        <span className="top-group">
+          <img src="favicon.png" className="logo-icon" alt="AP-Email" />
+          <strong>Inbox</strong>
+        </span>
+        <input type="text" placeholder="Search mail..." />
+      </div>
+
+      <div className="sidebar">
+        <p className="clickable">Primary</p>
+        <p className="clickable">Social</p>
+        <p className="clickable">Promotions</p>
+      </div>
+
+      <div className="main">
+        <div className="tabs">
+          <div className="tab active">Primary</div>
+          <div className="tab">Social</div>
+          <div className="tab">Promotions</div>
+        </div>
+
+        <div className="email-list">
+          <button className="email-row">
+            <span className="subject">ETL</span>
+            <span className="body">Equipment Return Instructions</span>
+            <span>Mar 31</span>
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
