bfea438111b0e24d21f870f58af73c585d80d299
OmerElmaliach <omerelmaliachprivate@gmail.com>
2025-06-14 21:25:02 +0300

    AE-157 Added buttons and information to emaildisplay

--- src/react-server/src/components/emailDisplay.js
+++ src/react-server/src/components/emailDisplay.js
@@ -6,11 +6,14 @@ import ErrorPage from './errorPage.js'
 import Topbar from './topbar.js'
 import Sidebar from './sidebar.js'
 import { useAppContext } from '../context/appContext.js';
+import { useNavigate } from 'react-router-dom';
 
 const EmailDisplay = () => {
+    const [showToast, setShowToast] = useState(false);
     const { error, setError } = useAppContext();
     const [email, setEmail] = useState([]);
     const { id } = useParams(); // Get email id.
+    const navigate = useNavigate();
 
     useEffect(() => {
         const loadData = async () => {
@@ -25,11 +28,28 @@ const EmailDisplay = () => {
         }
 
         loadData();
-    }, [id]);
+    }, [id, setError]);
+
+    // Reports an email as spam.
+    const reportSpam = (emailId) => {
+        // TODO
+    }
+
+    // Deletes an email.
+    const deleteEmail = (emailId) => {
+        // TODO
+    }
+
+    const showDeleteMessage = () => {
+        setShowToast(true);
+        setTimeout(() => {
+            setShowToast(false);
+        }, 2000);
+    };
 
     return (
         <>
-        {error != undefined ? (
+        {error ? (
             <ErrorPage/>
         ) : (
             <>
@@ -38,6 +58,33 @@ const EmailDisplay = () => {
 
             {/* Sidebar display */}
             <Sidebar/>
+
+            {/* Email information display */}
+            <div className="main">
+            {showToast && (
+                <div className="delete-toast">
+                    Email deleted successfully!
+                </div>
+            )}
+                <div className='toolbar-container'>
+                    <img src='../../misc/arrow_back_icon.png' className='toolbar-btn' onClick={() => navigate('/')} title='Back to Inbox' alt='Go back'/>
+                    <div className="vertical-divider"></div>
+                    <img src='../../misc/report_spam_icon.png' className='toolbar-btn' onClick={() => reportSpam(email.mail_id)} title='Report Spam' alt='Report Spam'/>
+                    <img src='../../misc/light_delete_icon.png' className='toolbar-btn' onClick={() => {
+                        showDeleteMessage()
+                        deleteEmail(email.id);
+                        setTimeout(() => {
+                            navigate('/');
+                          }, 750);
+                        }} title='Delete Email' alt='Delete Email'/>
+                </div>
+                <hr className="line-divider" />
+                <h1>{email.subject}</h1>
+                <p>
+                    From: {email.from} <br />
+                    Body: {email.body}
+                </p>
+            </div>
             </>
         )}
         </>
