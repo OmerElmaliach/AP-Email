1c96307e04d6d51d2e0e5d9058a7d44c06154af5
etlbro <eazbrody@gmail.com>
2025-06-19 16:09:32 +0300

    signin now looks good. yay

--- src/react-server/src/components/Auth/SignIn/UserPasswordInput.js
+++ src/react-server/src/components/Auth/SignIn/UserPasswordInput.js
@@ -1,33 +1,52 @@
+import './UserPasswordInput.css';
 
-function UserPasswordInput({ mailAdress, setEmailAdress, password, setPassword, handleUserSignIn}) {
- 
+function UserPasswordInput({ mailAdress, setEmailAdress, password, setPassword, handleUserSignIn }) {
   return (
-    <div className="form-floating">
-      <input
-        type="email"
-        value={mailAdress}
-        onChange={e => setEmailAdress(e.target.value)}
-        placeholder="Choose your email"
-        className="text-input"
-      />
+    <div className="signin-container">
+      <div className="signin-box">
+        <div className="signin-left">
+          <img src="/favicon.png" alt="Logo" className="logo-img" />
+        </div>
+        <div className="signin-right">
+          <h2>Sign in</h2>
+          <p className="subtext">to continue to AP-Mail</p>
 
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-        className="text-input"
-      />
+          <div className="form-floating mb-3 full-width">
+            <input
+              type="email"
+              className="form-control"
+              id="floatingSigninEmail"
+              placeholder="Email Address"
+              value={mailAdress}
+              onChange={e => setEmailAdress(e.target.value)}
+            />
+            <label htmlFor="floatingSigninEmail">Email Address</label>
+          </div>
 
-      <button
-    id="next-button"
-    className="btn btn-primary rounded-pill align-right"
-    onClick={handleUserSignIn}
-    disabled={!mailAdress || !password}
-  >
-    signin
-  </button>
+          <div className="form-floating mb-3 full-width">
+            <input
+              type="password"
+              className="form-control"
+              id="floatingSigninPassword"
+              placeholder="Password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <label htmlFor="floatingSigninPassword">Password</label>
+          </div>
+
+          <div className="button-row">
+            <button
+              className="btn btn-primary rounded-pill"
+              onClick={handleUserSignIn}
+              disabled={!mailAdress || !password}
+            >
+              Sign In
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-export default UserPasswordInput
+export default UserPasswordInput;
