87349c44f95461ddf323994809b63b7bbc71e932
etlbro <eazbrody@gmail.com>
2025-06-21 23:48:32 +0300

    bug ffiex, added short delay so bootstarp can load

--- src/react-server/src/signUp/SignUp.js
+++ src/react-server/src/signUp/SignUp.js
@@ -15,11 +15,12 @@ import EmailPasswordStep from './SignUpSteps/EmailPasswordStep';
 
 // Main signup flow component
 function SignUp() {
-   // Dynamically add Bootstrap CSS only when SignUp is mounted. note to self- DONT USE BOOT STRAP AGAIN!!!!!
+  // Dynamically add Bootstrap CSS only when SignUp is mounted. note to self- DONT USE BOOT STRAP AGAIN!!!!!
   useEffect(() => {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css';
+    link.onload = () => setBootstrapReady(true);
     document.head.appendChild(link);
 
     return () => {
@@ -27,7 +28,12 @@ function SignUp() {
     };
   }, []);
 
+
+
   const navigate = useNavigate();
+
+  const [bootstrapReady, setBootstrapReady] = useState(false);
+
   //helper functions for page navigation 
   const [loading, setLoading] = useState(false);
   const [step, setStep] = useState(1);
@@ -35,17 +41,20 @@ function SignUp() {
   const [firstName, setFirstName] = useState(''); // get first name
   const [lastName, setLastName] = useState(''); // get last name
   const [picture, setPicture] = useState(null); // get pic
-  const [gender , setGender] = useState(''); // get gender
+  const [gender, setGender] = useState(''); // get gender
   const [birthday, setBirthDay] = useState(''); // get birthday
   const [mailAdress, setEmailAdress] = useState(''); // get email
-  const [password, setPassword]  = useState(''); // get password
+  const [password, setPassword] = useState(''); // get password
 
+  if (!bootstrapReady) {
+    return null; // white screen during Bootstrap load
+  }
   
   const nextStep = () => setStep(step + 1);
   const prevStep = () => setStep(step - 1);
 
 
-  const handleCreateUser =async ()=> {
+  const handleCreateUser = async () => {
     setLoading(true);
     const formData = new FormData();
     formData.append('firstName', firstName);
@@ -59,79 +68,79 @@ function SignUp() {
       formData.append('picture', picture);
     }
 
-     try {
-    const success = await ApiService.signupUser(formData);
-    if (success) navigate('/inbox');
-    else{
-     alert('Signup failed');
+    try {
+      const success = await ApiService.signupUser(formData);
+      if (success) navigate('/inbox');
+      else {
+        alert('Signup failed');
+      }
+    } catch (err) {
+
+      alert(err.message || 'Something went wrong');
+
+    } finally {
+      setLoading(false);
     }
-  } catch (err) {
-
-    alert(err.message || 'Something went wrong');
-
-  } finally {
-    setLoading(false);
   }
- }
 
   return (
-     
-  <div className="signup-container">
-    <div className="signup-box">
-      
-      {/* Left side with logo */}
-      <div className="signup-left">
-        <img src="/favicon.png" alt="Logo" className="logo-img" />
+
+    <div className="signup-container">
+      <div className="signup-box">
+
+        {/* Left side with logo */}
+        <div className="signup-left">
+          <img src="/favicon.png" alt="Logo" className="logo-img" />
+        </div>
+
+        {/* Right side with form step */}
+        <div className="signup-right">
+          {step === 1 && (
+            <NameStep
+              firstName={firstName}
+              setFirstName={setFirstName}
+              lastName={lastName}
+              setLastName={setLastName}
+              onNext={nextStep}
+            />
+          )}
+
+          {step === 2 && (
+            <PictureStep
+              picture={picture}
+              setPicture={setPicture}
+              onNext={nextStep}
+              onBack={prevStep}
+            />
+          )}
+
+          {step === 3 && (
+            <BirthdayStep
+              gender={gender}
+              setGender={setGender}
+              birthday={birthday}
+              setBirthDay={setBirthDay}
+              onNext={nextStep}
+              onBack={prevStep}
+            />
+          )}
+
+          {step === 4 && (
+            <EmailPasswordStep
+              mailAdress={mailAdress}
+              setEmailAdress={setEmailAdress}
+              password={password}
+              setPassword={setPassword}
+              handleCreateUser={handleCreateUser}
+              loading={loading}
+              setLoading={setLoading}
+              onBack={prevStep}
+            />
+          )}
+        </div>
+
       </div>
-
-      {/* Right side with form step */}
-      <div className="signup-right">
-        {step === 1 && (
-          <NameStep 
-            firstName={firstName} 
-            setFirstName={setFirstName} 
-            lastName={lastName} 
-            setLastName={setLastName} 
-            onNext={nextStep}
-          />
-        )}
-
-        {step === 2 && (
-          <PictureStep 
-            picture={picture} 
-            setPicture={setPicture} 
-            onNext={nextStep} 
-            onBack={prevStep} 
-          />
-        )}
-
-        {step === 3 && (
-          <BirthdayStep  
-            gender={gender} 
-            setGender={setGender} 
-            birthday={birthday}
-            setBirthDay={setBirthDay} 
-            onNext={nextStep} 
-            onBack={prevStep}
-          />
-        )}
-
-        {step === 4 && (
-          <EmailPasswordStep 
-            mailAdress={mailAdress} 
-            setEmailAdress={setEmailAdress}  
-            password={password}
-            setPassword={setPassword} 
-            handleCreateUser={handleCreateUser} 
-            loading={loading}
-            setLoading={setLoading}
-            onBack={prevStep}
-          />
-        )}
-      </div>
-
     </div>
-  </div>
   );
 }
 
