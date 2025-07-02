e5c14efa6f2422b26e8f9f4cab5c28d9131144e4
Omer Elmaliach <omerelmaliachprivate@gmail.com>
2025-07-02 14:09:42 +0300

    AE-198 Added email recyclerview

--- src/android/app/src/main/java/com/example/ap_emailandroid/ui/InboxActivity.java
+++ src/android/app/src/main/java/com/example/ap_emailandroid/ui/InboxActivity.java
@@ -6,8 +6,15 @@ import com.example.ap_emailandroid.R;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ap_emailandroid.ui.email.Email;
+import com.example.ap_emailandroid.ui.email.EmailAdapter;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class InboxActivity extends AppCompatActivity {
@@ -30,5 +37,21 @@ public class InboxActivity extends AppCompatActivity {
 
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        // TODO: ADD DYNAMIC EMAILS
+        RecyclerView emailList = findViewById(R.id.email_list);
+        emailList.setLayoutManager(new LinearLayoutManager(this));
+
+        List<Email> emails = Arrays.asList(
+                new Email("Test Email 1", "Thanks for joining AP-Email."),
+                new Email("Test Email 2", "Check out the new features."),
+                new Email("Test Email 3", "We detected a login from a new device.")
+        );
+
+        EmailAdapter adapter = new EmailAdapter(emails, email -> {
+            // TODO: ADD ONCLICK EVENT
+        });
+
+        emailList.setAdapter(adapter);
     }
 }
