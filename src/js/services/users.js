351b56887df7e562f71d23b74b065b197b91c5fd
etlbro <eazbrody@gmail.com>
2025-07-04 01:42:04 +0300

    added coment to users services

--- src/js/services/users.js
+++ src/js/services/users.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-const User = require('../models/users'); // your Mongoose model
+const User = require('../models/users'); // the mongoose users model
 
 
 //add user to data base
