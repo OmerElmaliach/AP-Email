91073cd373efac3fe9c6883da55da5ce0610b362
etlbro <eazbrody@gmail.com>
2025-07-04 00:25:09 +0300

    add users BD with updated models and new services folder

--- src/js/models/users.js
+++ src/js/models/users.js
@@ -1,41 +1,22 @@
+const mongoose = require('mongoose');
 
 
-const users_DB = [] 
+const users = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  userName: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  birthday: { type: Date, required: true },
+  phoneNumber: { type: String, default: null }, // optional, ended up not using it in the end
+  gender: { type: String, default: 'Prefer not to say' },
+  picture: { type: String, required: true },
+  labels: { type: Array, default: [] }, // optional array, default empty
+});
 
-let currentId = 1;
-//add user to data base- in our case the array and set its id 
-const addUser = (user) =>{
-    user.id = currentId.toString()
-    currentId++
-    users_DB.push(user)
-}
+
+module.exports = mongoose.model('Users', users);
 
 
-  /* function to get a user- enter key *string* to search and what val for that key 
-   **MUST CHECK RETURN VAL** 
-   */
-const getUser = (field, value) => {
-  if (value === undefined || value === null || value === '') return undefined;
-  return users_DB.find(user => user[field] == value);
-};
 
-/*
-updates a field of a user in users_DB
-parameters:
-id: User ID to find
-field: Field name to update (string)
-valueUpdate: New value for the field
-*/
-const updateValue = (id , field, valueUpdate)=>{
-  const updatedUser = users_DB.find(user => user.id === id)
-  if(updatedUser){
-  updatedUser[field] = valueUpdate
-}
-}
 
-/* for debugging- get all user
-const getAllUsers = () => {
-  return users_DB
-}*/
-
-module.exports= { addUser, getUser ,updateValue}
