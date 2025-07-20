1fee40fa223ad825478d21dd777b00efb4993680
Omer Elmaliach <omerelmaliachprivate@gmail.com>
2025-07-20 21:50:59 +0300

    AE-200 Added token bearer to header && add label button

--- src/android/app/src/main/java/com/example/ap_emailandroid/network/EmailAPI.java
+++ src/android/app/src/main/java/com/example/ap_emailandroid/network/EmailAPI.java
@@ -9,6 +9,8 @@ import com.example.ap_emailandroid.AppController;
 import com.example.ap_emailandroid.R;
 import com.example.ap_emailandroid.local.Email;
 import com.example.ap_emailandroid.local.EmailDao;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -21,23 +23,27 @@ import java.util.function.Consumer;
 public class EmailAPI {
     private MutableLiveData<List<Email>> emailListData;
     private EmailDao dao;
-    private final String userId;
     Retrofit retrofit;
     WebServiceAPI webServiceAPI;
 
     public EmailAPI(MutableLiveData<List<Email>> emailListData, EmailDao dao, String userId) {
-        this.userId = userId;
         this.emailListData = emailListData;
         this.dao = dao;
 
-        retrofit = new Retrofit.Builder().baseUrl(AppController.context.getString(R.string.BaseUrl))
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new AuthInterceptor(userId))
+                .build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(AppController.context.getString(R.string.BaseUrl))
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         webServiceAPI = retrofit.create(WebServiceAPI.class);
     }
 
     public void get() {
-        Call<List<Email>> call = webServiceAPI.getEmails(userId);
+        Call<List<Email>> call = webServiceAPI.getEmails();
         call.enqueue(new Callback<>() {
             @Override
             public void onResponse(@NonNull Call<List<Email>> call, @NonNull Response<List<Email>> res) {
@@ -83,7 +89,7 @@ public class EmailAPI {
     }
 
     public void update(Email email, Consumer<Email> onSuccess) {
-        Call<Email> call = webServiceAPI.updateEmail(email.getMailId(), email, userId);
+        Call<Email> call = webServiceAPI.updateEmail(email.getMailId(), email);
         call.enqueue(new Callback<>() {
             @Override
             public void onResponse(@NonNull Call<Email> call, @NonNull Response<Email> res) {
@@ -104,7 +110,7 @@ public class EmailAPI {
     }
 
     public void delete(Email email) {
-        Call<Void> call = webServiceAPI.deleteEmail(email.getMailId(), userId);
+        Call<Void> call = webServiceAPI.deleteEmail(email.getMailId());
         call.enqueue(new Callback<>() {
             @Override
             public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> res) {
