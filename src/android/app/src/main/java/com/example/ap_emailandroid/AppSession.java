f65d70bca0aecccfe330ed661acf6e482ef02364
Gavriel-Cohen <gavriel.cohen@live.biu.ac.il>
2025-07-22 21:11:53 +0300

    user photo menu + profile picture added

--- src/android/app/src/main/java/com/example/ap_emailandroid/AppSession.java
+++ src/android/app/src/main/java/com/example/ap_emailandroid/AppSession.java
@@ -2,4 +2,8 @@ package com.example.ap_emailandroid;
 
 public class AppSession {
     public static String userId;
+    public static String userToken;
+    public static String userEmail;
+    public static String userFullName;
+    public static String userPicture;
 }
